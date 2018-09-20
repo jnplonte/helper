e1a7734d3df0b2a72f4c6c1f5e116ab660b40c95
John Paul Onte <johnpaul@novafabrik.com>
2018-09-20 11:25:52 +0800

    fix filter data

--- example/index.js
+++ example/index.js
@@ -1,5 +1,22 @@
 const helper = require("../index");
 
-console.log(
-    helper.dateNow, helper.dateTimeNow, helper.dateTimeNowEnd, helper.dateTimeNowStart 
-);
+const data =  [
+    {
+        "SalesTable": {
+            "Estate": {
+                "RecId": 1,
+                "EstateName": "dsad 1"
+            }
+        }
+    },
+    {
+        "SalesTable": {
+            "Estate": {
+                "RecId": 1,
+                "EstateName": "dsad 1"
+            }
+        }
+    }
+];
+const x = helper.filterData(data, ["SalesTable.Estate"])
+console.log(JSON.stringify(x));
